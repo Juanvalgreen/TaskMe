ee9b45faaf02a1ac748276081ee321ac3e2ba40e
Juanvalgreen <108194158+Juanvalgreen@users.noreply.github.com>
2023-06-14 12:55:05 -0500

    Initial commit, all pending first page in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import SideBar from './Components/SideBar';
+import TaskCard from './Components/TaskCard';
 
 function App() {
 
   return (
     <>
-      
+      <SideBar/>
+      <TaskCard/>
+    
+    
     </>
   )
 }
---
+++ src/Components/TaskCard.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+
+
+function TaskCard(){
+
+
+
+
+
+
+
+
+
+
+
+
+    return (
+        <>
+            <div className="max-w-xs max-h-96 overflow-hidden border rounded-2xl shadow bg-slate-900 border-gray-800 cursor-pointer transform transition duration-300 hover:scale-105 justify-center">
+                <div className="px-6 py-4">
+                    <h5 className="text-white mb-2 text-base tracking-tight capitalize truncate text-center">
+                        Hola
+                    </h5>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default TaskCard;
